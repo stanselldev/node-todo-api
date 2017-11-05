16687fbf52a2a944228fa1b7c76196ff6bf40ab1
cameron stansell <cameronstansell@camerons-MacBook-Pro.local>
2017-11-05 14:55:45 -0700

    Added GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -30,7 +31,23 @@ app.get('/todos', (req, res) => {
   });
 });
 
+app.get('/users/:id', (req, res) => {
+  var id = req.params.id;
 
+  if (!ObjectID.isValid(id)) {
+    res.status(404).send('ID not valid');
+  };
+
+  User.findById(id).then((user) => {
+    if (!user) {
+      res.status(404).send('User not found');
+    }
+
+    res.status(200).send({user});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
