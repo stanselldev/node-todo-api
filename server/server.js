581212180a7bd8104b58b69fb6918de2c29ffc87
cameron stansell <cameronstansell@camerons-MacBook-Pro.local>
2017-11-12 20:33:15 -0700

    setup app for Heroku

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -31,26 +32,26 @@ app.get('/todos', (req, res) => {
   });
 });
 
-app.get('/users/:id', (req, res) => {
+app.get('/todos/:id', (req, res) => {
   var id = req.params.id;
 
   if (!ObjectID.isValid(id)) {
     res.status(404).send('ID not valid');
   };
 
-  User.findById(id).then((user) => {
-    if (!user) {
-      res.status(404).send('User not found');
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      res.status(404).send('Todo not found');
     }
 
-    res.status(200).send({user});
+    res.status(200).send({todo});
   }).catch((e) => {
     res.status(400).send();
   });
 });
 
-app.listen(3000, () => {
-  console.log('Started on port 3000');
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
 });
 
 module.exports = {app};
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,20 +1,22 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 const todos = [{
+  _id: new ObjectID(),
   text: 'First test todo'
 }, {
+  _id: new ObjectID(),
   text: 'Second test todo'
 }];
-
 
 beforeEach((done) => {
   Todo.remove({}).then(() => {
     return Todo.insertMany(todos);
-  }).then(() => done())
+  }).then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -59,7 +61,7 @@ describe('POST /todos', () => {
   });
 });
 
-describe('Get /todos', () => {
+describe('GET /todos', () => {
   it('should get all todos', (done) => {
     request(app)
       .get('/todos')
@@ -68,5 +70,33 @@ describe('Get /todos', () => {
         expect(res.body.todos.length).toBe(2);
       })
       .end(done);
-  })
+  });
+});
+
+describe('GET /todos/:id', () => {
+  it('should return todo doc', (done) => {
+    request(app)
+      .get(`/todos/${todos[0]._id.toHexString()}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(todos[0].text);
+      })
+      .end(done);
+  });
+
+  it('should return 404 if todo not found', (done) => {
+    var hexId = new ObjectID().toHexString();
+
+    request(app)
+      .get(`/todos/${hexId}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return 404 for non-object ids', (done) => {
+    request(app)
+      .get('/todos/123abc')
+      .expect(404)
+      .end(done);
+  });
 });
